70d63fa327f54415e77468ab76ea5b1f7c742d9b
Денис <akimovdo@gmail.com>
2025-11-12 20:29:45 +0300

    fix

---
+++ pb_hooks/pages/+config.js
@@ -0,0 +1,17 @@
+/**
+ * Конфигурация PocketPages
+ */
+module.exports = {
+  plugins: [
+    'pocketpages-plugin-ejs',
+    { 
+      name: 'pocketpages-plugin-js-sdk', 
+      host: process.env.PP_HOST || 'http://127.0.0.1:8090' 
+    },
+    { 
+      name: 'pocketpages-plugin-auth', 
+      collection: 'users' 
+    },
+  ],
+  debug: false,
+};
