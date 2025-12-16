807962df18547977ea1fbccc339c43e8c9e0f6d2
Денис <akimovdo@gmail.com>
2025-12-16 23:58:36 +0300

    fix

--- pb_hooks/pages/+config.js
+++ pb_hooks/pages/+config.js
@@ -4,6 +4,7 @@
 module.exports = {
   plugins: [
     'pocketpages-plugin-ejs',
+    'pocketpages-plugin-datastar',
     { 
       name: 'pocketpages-plugin-js-sdk', 
       host: process.env.PP_HOST || 'http://127.0.0.1:8090' 
